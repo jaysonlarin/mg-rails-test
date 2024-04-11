540a24c77db7d2d7e6cccf67ae4ffe4dcb4dcef3
Jayson Larin <jaysonlarin@yahoo.com>
2024-04-12 01:19:04 +0800

    Implement an asc/desc sort function

--- app/controllers/basket_case_controller.rb
+++ app/controllers/basket_case_controller.rb
@@ -1,16 +1,23 @@
 class BasketCaseController < ApplicationController
   before_action :set_fruits
   before_action :set_items
+  before_action :set_order
 
   def index; end
 
   def order_by_fruit
     @list_items = @list_items.sort
-    render 'basket_case/index'
+    reverse_and_render
   end
 
   def order_by_amount
     @list_items = @list_items.sort_by(&:last)
+    reverse_and_render
+  end
+
+  def reverse_and_render
+    @list_items = @list_items.reverse if params.has_key?(:order) && params[:order] == "desc"
+
     render 'basket_case/index'
   end
 
@@ -26,5 +33,10 @@ class BasketCaseController < ApplicationController
 
     def set_items
       @list_items = @fruits
+    end
+
+    def set_order
+      return @order = 'desc' unless params.has_key?(:order)
+      @order = params[:order] == 'desc' ? 'asc' : 'desc'
     end
 end
